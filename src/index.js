3c9ac828fdbb0a81cc239a6c8c742526e076560f
Nnvedward <nnvedward@gmail.com>
2022-02-16 10:09:25 +0100

    Setup WebSocket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const { Server } = require("socket.io")
 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = new Server(server)
 
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
+
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
